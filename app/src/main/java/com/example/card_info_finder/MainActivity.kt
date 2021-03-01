e4c60fdd20509ae31a8eae46bb6999374fefd201
ayodeletolulope18 <ayodeletolulope18@gmail.com>
2021-03-01 18:52:29 +0100

    initial commit

---
+++ app/src/main/java/com/example/card_info_finder/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.card_info_finder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
